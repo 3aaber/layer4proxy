9e3ee4706c151e6e71700e064402d3b6ae3b8a48
Saber Mesgari <saber.mesgari@gmail.com>
2022-12-06 15:49:36 +0330

    add udp server to testutils

--- testutils/udpserver.go
+++ testutils/udpserver.go
@@ -1,1 +1,29 @@
 package testutils
+
+import (
+	"fmt"
+	"net"
+)
+
+func UDPServer(ip string, port int, connChan chan net.Conn) (net.Listener, error) {
+
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{
+		Port: port,
+		IP:   []byte(ip),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	fmt.Printf("server listening %s\n", conn.LocalAddr().String())
+
+	for {
+		message := make([]byte, 20)
+		_, _, err := conn.ReadFromUDP(message[:])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+	}
+}
